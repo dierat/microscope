f04299d43ff8367b650560f98cdd67a0e5ed8a76
dierat <diedra.rater@gmail.com>
2015-02-27 19:19:35 -0800

    adds notifications

--- client/mongol_code.js
+++ client/mongol_code.js
@@ -1,5 +1,5 @@
 Session.set("Mongol", {
-	'collections': ['Posts'],
+	'collections': ['Posts', 'Comments', 'Notifications'],
     'display': true,
     'opacity_normal': ".7",
     'opacity_expand': ".9",
